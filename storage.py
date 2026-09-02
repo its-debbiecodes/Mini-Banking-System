0e50f82c3d0127e677c0bfa15be666f87391af77
its-debbiecodes <debbieabegunde23@gmail.com>
2026-09-02 11:57:37 +0100

    git commit -m "Added an automatic account number counter"

--- storage.py
+++ storage.py
@@ -1,23 +1,24 @@
 import json
-bank_database= []
-try:
-    with open("bank_data.json", "r") as f:
-        bank_database =json.load(f)
-except FileNotFoundError:
-    print("Bank data file not found, starting with empty database")
-    bank_database = []
-except json.decoder.JSONDecodeError:
-    print("Warning: bank_data.json missing or corrupt. starting a new file")
-    bank_database = []
+from Tasks import BankAccount
+import Tasks
+def load_bank_data():
+    try:
+        with open("bank_data.json", "r") as f:
+            raw_data =json.load(f)
+            Tasks.bank_database =[
+                BankAccount(d["holder"], d["balance"], d["account_number"])
+                for d in raw_data
+            ]
+            BankAccount.total_bank_balance =sum(a.balance for a in Tasks.bank_database)
+    except FileNotFoundError:
+        print("Bank data file not found, starting with empty database")
+        Tasks.bank_database = []
+    except json.decoder.JSONDecodeError:
+        print("Warning: bank_data.json missing or corrupt. starting a new file")
+        Tasks.bank_database = []
 
-def update_bank_data(striped_bank_database):
+
+def save_bank_data():
+    striped_bank_database = [account.__dict__ for account in Tasks.bank_database]
     with open("bank_data.json", "w") as file:
         json.dump(striped_bank_database, file, indent=4)
-        return striped_bank_database
-
-def get_bank_data():
-    striped_bank_database = []
-    for account in bank_database:
-        striped_data=account.__dict__
-        striped_bank_database.append(striped_data)
-    update_bank_data(striped_bank_database)
